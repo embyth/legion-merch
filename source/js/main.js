50bf90f4317ea9cac5f04e18543de4a403b45e50
Rostyslav Miniukov <thresh1337@gmail.com>
2020-03-12 17:25:28 +0200

    Добавил базовый скрипт для начала

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,22 @@
 'use strict';
 
-// Shadow SVG for IE11
+// SVG Sprite
 svg4everybody(); // eslint-disable-line
+
+// JavaScript Page Toggler
+const pageBody = document.querySelector('.page');
+if (pageBody.classList.contains('page--no-js')) {
+  pageBody.classList.remove('page--no-js');
+}
+
+// Menu Toggler
+const toggleButton = document.querySelector('.menu-toggler');
+const siteNavigation = document.querySelector('.site-nav');
+
+if (toggleButton) {
+  toggleButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    siteNavigation.classList.toggle('site-nav--closed');
+    this.classList.toggle('menu-toggler--opened');
+  });
+}
