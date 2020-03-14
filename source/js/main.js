15fd7eb1b552015721dc3a5fde06d8fb2cd9889d
Rostyslav Miniukov <thresh1337@gmail.com>
2020-03-14 23:09:49 +0200

    Дополнил открывашку для меню и добавил скрипт для добавления атрибутов

--- source/js/main.js
+++ source/js/main.js
@@ -10,13 +10,34 @@ if (pageBody.classList.contains('page--no-js')) {
 }
 
 // Menu Toggler
-const toggleButton = document.querySelector('.menu-toggler');
-const siteNavigation = document.querySelector('.site-nav');
+let menuButtons = document.querySelectorAll('.menu-toggler');
+let menuClose = document.querySelector('.site-nav__close-button');
+let menuOverlay = document.querySelector('.site-nav__overlay');
 
-if (toggleButton) {
-  toggleButton.addEventListener('click', function (event) {
+for (let i = 0; i < menuButtons.length; i++) {
+  menuButtons[i].addEventListener('click', function (event) {
     event.preventDefault();
-    siteNavigation.classList.toggle('site-nav--closed');
-    this.classList.toggle('menu-toggler--opened');
+    menuButtons[i].classList.toggle('menu-toggler--opened');
   });
+
+  menuClose.addEventListener('click', function (event) {
+    event.preventDefault();
+    menuButtons[i].classList.toggle('menu-toggler--opened');
+  });
+
+  menuOverlay.addEventListener('click', function (event) {
+    event.preventDefault();
+    menuButtons[i].classList.toggle('menu-toggler--opened');
+  });
+}
+
+
+// Creating attributes for buttons
+let glitchButtons = document.querySelectorAll('.button--glitch');
+
+for (let i = 0; i < glitchButtons.length; i++) {
+  let content = glitchButtons[i].textContent.toUpperCase();
+  let attr = document.createAttribute('data-text');
+  attr.value = content;
+  glitchButtons[i].setAttributeNode(attr);
 }
