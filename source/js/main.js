b9c1b3d5b0bb8d759af445a59df60a6b962f72c5
Rostyslav Miniukov <thresh1337@gmail.com>
2020-03-16 23:02:07 +0200

    Переместил скрипт для попапов с файла на страницы инлайном

--- source/js/main.js
+++ source/js/main.js
@@ -9,29 +9,6 @@ if (pageBody.classList.contains('page--no-js')) {
   pageBody.classList.remove('page--no-js');
 }
 
-// Menu Toggler
-let menuButtons = document.querySelectorAll('.menu-toggler');
-let menuClose = document.querySelector('.site-nav__close-button');
-let menuOverlay = document.querySelector('.site-nav__overlay');
-
-for (let i = 0; i < menuButtons.length; i++) {
-  menuButtons[i].addEventListener('click', function (event) {
-    event.preventDefault();
-    menuButtons[i].classList.toggle('menu-toggler--opened');
-  });
-
-  menuClose.addEventListener('click', function (event) {
-    event.preventDefault();
-    menuButtons[i].classList.toggle('menu-toggler--opened');
-  });
-
-  menuOverlay.addEventListener('click', function (event) {
-    event.preventDefault();
-    menuButtons[i].classList.toggle('menu-toggler--opened');
-  });
-}
-
-
 // Creating attributes for buttons
 let glitchButtons = document.querySelectorAll('.button--glitch');
 
