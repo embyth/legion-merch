8477ac57ac70617eb52a750d7b3b9e5bd7d2ee1e
Rostyslav Miniukov <thresh1337@gmail.com>
2020-08-04 23:32:32 +0300

    Добавил закрытие попапа корзины с помощью кнопки ESC

--- source/js/_cart.js
+++ source/js/_cart.js
@@ -11,43 +11,48 @@
     cartBody.classList.add('cart--open');
     cartOverlay.classList.add('cart__overlay--visible');
     window.utils.hideBodyScroll();
+
+    const cartClose = cartBody.querySelector('.cart__close');
+    cartClose.addEventListener('click', cartCloseClickHandler);
+    cartOverlay.addEventListener('click', cartCloseClickHandler);
+    document.addEventListener('keydown', cartCloseKeyKodeHandler);
   }
 
   function cartCloseClickHandler(evt) {
     evt.preventDefault();
-    var target = evt.target;
     const cartBody = document.getElementById('cart-pop');
     const cartOverlay = document.querySelector('.cart__overlay');
     const cartClose = cartBody.querySelector('.cart__close');
 
-    if (target === cartClose || target === cartOverlay) {
-      cartBody.classList.remove('cart--open');
-      cartOverlay.classList.remove('cart__overlay--visible');
-      window.utils.showBodyScroll();
+    cartBody.classList.remove('cart--open');
+    cartOverlay.classList.remove('cart__overlay--visible');
+    window.utils.showBodyScroll();
+
+    cartClose.removeEventListener('click', cartCloseClickHandler);
+    cartOverlay.removeEventListener('click', cartCloseClickHandler);
+    document.removeEventListener('keydown', cartCloseKeyKodeHandler);
+  }
+
+  function cartCloseKeyKodeHandler(evt) {
+    if (window.utils.isEscPressed(evt)) {
+      cartCloseClickHandler(evt);
     }
   }
 
   function activate() {
     const cartToggler = document.querySelector('.user-nav__button--cart');
     const cartBody = document.getElementById('cart-pop');
-    const cartClose = cartBody.querySelector('.cart__close');
-    const cartOverlay = document.querySelector('.cart__overlay');
 
     cartToggler.addEventListener('click', cartOpenClickHandler);
-    cartClose.addEventListener('click', cartCloseClickHandler);
-    cartOverlay.addEventListener('click', cartCloseClickHandler);
+
     window.resize.activate(cartBody);
   }
 
   function disable() {
     const cartToggler = document.querySelector('.user-nav__button--cart');
     const cartBody = document.getElementById('cart-pop');
-    const cartClose = cartBody.querySelector('.cart__close');
-    const cartOverlay = document.querySelector('.cart__overlay');
 
     cartToggler.removeEventListener('click', cartOpenClickHandler);
-    cartClose.removeEventListener('click', cartCloseClickHandler);
-    cartOverlay.removeEventListener('click', cartCloseClickHandler);
     window.resize.disable(cartBody);
   }
 
