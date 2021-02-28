e438d730bca289519c89c912daebfd0283ce77cd
Rostyslav Miniukov <thresh1337@gmail.com>
2021-02-28 16:27:18 +0200

    Создал моковые данные для товаров. WIP

---
+++ src/helpers/my-types.ts
@@ -0,0 +1,58 @@
+export interface ProductInterface {
+  name: string;
+  description: string;
+  alias: string;
+  id: number;
+  category: string;
+  color: string;
+  isAvailable: boolean;
+  about: {
+    comp: string;
+    wash: string;
+  };
+  price: {
+    isOnSale: boolean;
+    value: number;
+    prevValue: number;
+    currency: string;
+  };
+  sizes: {
+    s?: {
+      stock: number;
+      label: string;
+    };
+    m?: {
+      stock: number;
+      label: string;
+    };
+    l?: {
+      stock: number;
+      label: string;
+    };
+    xl?: {
+      stock: number;
+      label: string;
+    };
+    xxl?: {
+      stock: number;
+      label: string;
+    };
+    onesize?: {
+      stock: number;
+      label: string;
+    };
+    women?: {
+      stock: number;
+      label: string;
+    };
+    men?: {
+      stock: number;
+      label: string;
+    };
+  };
+  collection: {
+    name: string;
+    id: number;
+  };
+  pictures: Array<string>;
+}
