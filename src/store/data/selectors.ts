5346ce0c4764616fb1517af95bec82f4566e4cba
Rostyslav Miniukov <thresh1337@gmail.com>
2021-03-05 21:40:32 +0200

    Обновил дата селекторы под reselect и добавил селектор для получение категорий

--- src/store/data/selectors.ts
+++ src/store/data/selectors.ts
@@ -1,3 +1,5 @@
+import {createSelector} from "reselect";
+
 import NameSpace from "../name-space";
 
 export const getProducts = (state) => state[NameSpace.DATA].products;
@@ -5,17 +7,38 @@ export const getProductsRequestStatus = (state) => state[NameSpace.DATA].product
 export const getIsLoading = (state) => state[NameSpace.DATA].isLoading;
 export const getIsLoadError = (state) => state[NameSpace.DATA].isLoadError;
 
-export const getProductByAlias = (state, ownProps) => {
-  const products = getProducts(state);
-  const productAlias = ownProps.routeProps.match.params.alias;
-  return products.find((product) => product.alias === productAlias);
-};
+const getExtraProps = (state, extraProps) => extraProps;
 
-export const getProductsByCategory = (state, ownProps) => {
-  const products = getProducts(state);
-  const productsCategory = ownProps.routeProps.match.params.category;
+export const getProductByAlias = createSelector(
+    getProducts,
+    getExtraProps,
+    (products, ownProps) => {
+      const productAlias = ownProps.routeProps.match.params.alias;
+      return products.find((product) => product.alias === productAlias)
+    }
+);
 
-  return productsCategory
-    ? products.filter((product) => product.category.alias === productsCategory)
-    : products;
-};
+export const getProductsByCategory = createSelector(
+    getProducts,
+    getExtraProps,
+    (products, ownProps) => {
+      const productsCategory = ownProps.routeProps.match.params.category;
+
+      return productsCategory
+        ? products.filter((product) => product.category.alias === productsCategory)
+        : products;
+    }
+);
+
+export const getProductsCategories = createSelector(
+    getProducts,
+    (products) => {
+      return products
+        .filter((item, index, arr) =>
+          index === arr.findIndex((temp) =>
+            temp.category.alias === item.category.alias
+          )
+        )
+        .map((item) => item.category);
+    }
+);
