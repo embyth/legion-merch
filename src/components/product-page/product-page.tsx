8f90cfab99aed7587b283185012f74ab1263d985
Rostyslav Miniukov <thresh1337@gmail.com>
2021-03-07 21:44:39 +0200

    Подключил редьюсер корзины к компонентам товара и боковой корзины

--- src/components/product-page/product-page.tsx
+++ src/components/product-page/product-page.tsx
@@ -8,6 +8,7 @@ import {
   getProductByAlias,
   getProductsRequestStatus,
 } from "../../store/data/selectors";
+import {Operations as CartOperations} from "../../store/cart/cart";
 
 import Breadcrumbs from "../breadcrumbs/breadcrumbs";
 
@@ -25,167 +26,200 @@ interface ProductPageProps {
     isTablet: boolean;
     isMobile: boolean;
   };
+  onAddToCartButtonClick(productId: number, size: string): void;
 }
 
-const ProductPage: React.FC<ProductPageProps> = ({
-  product,
-  productsRequestStatus,
-  mediaQueries,
-}: ProductPageProps) => {
-  const {
-    DESKTOP: desktopSwiperParams,
-    MOBILE: mobileSwiperParams,
-  } = productCarouselParams;
-  const {isDesktop, isTablet, isMobile} = mediaQueries;
+interface ProductPageState {
+  currentSize: string;
+}
 
-  if (productsRequestStatus !== RequestStatus.SUCCESS) {
-    return <div>Loading...</div>;
+class ProductPage extends React.PureComponent<ProductPageProps, ProductPageState> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentSize: `not-selected`,
+    };
+
+    this.handleSizeSelectChange = this.handleSizeSelectChange.bind(this);
   }
 
-  return (
-    <React.Fragment>
-      <main className="main-content" id="main-content">
-        <h1 className="visually-hidden">
-          {`${product.type} цвета ${product.color} «${product.name}»`}
-        </h1>
+  private handleSizeSelectChange(evt) {
+    this.setState({
+      currentSize: evt.target.value,
+    });
+  }
 
-        <section className="product">
-          <h2 className="visually-hidden">Информация о товаре</h2>
-          <div className="product__inner">
-            {isMobile && <Breadcrumbs product={product} />}
+  render() {
+    const {
+      DESKTOP: desktopSwiperParams,
+      MOBILE: mobileSwiperParams,
+    } = productCarouselParams;
+    const {mediaQueries, productsRequestStatus, product, onAddToCartButtonClick} = this.props;
+    const {currentSize} = this.state;
+    const {isDesktop, isTablet, isMobile} = mediaQueries;
 
-            {isDesktop ? (
-              <Swiper
-                className="product__image-gallery"
-                direction={desktopSwiperParams.direction}
-                slidesPerView={desktopSwiperParams.slidesPerView}
-                allowTouchMove={desktopSwiperParams.allowTouchMove}
-                spaceBetween={desktopSwiperParams.spaceBetween}
-              >
-                {product.pictures.map((pic, index) => (
-                  <SwiperSlide key={pic}>
-                    <img
-                      src={pic}
-                      alt={`${product.type} цвета ${product.color} «${
-                        product.name
-                      }», ${index + 1} из ${product.pictures.length}`}
-                    />
-                  </SwiperSlide>
-                ))}
-                <div className="product__gallery-controls"></div>
-              </Swiper>
-            ) : (
-              <Swiper
-                className="product__image-gallery"
-                pagination={mobileSwiperParams.pagination}
-                a11y={mobileSwiperParams.a11y}
-                effect={mobileSwiperParams.effect}
-                direction={mobileSwiperParams.direction}
-                grabCursor={mobileSwiperParams.grabCursor}
-                slidesPerView={mobileSwiperParams.slidesPerView}
-                loop={mobileSwiperParams.loop}
-                autoHeight={mobileSwiperParams.autoHeight}
-              >
-                {product.pictures.map((pic, index) => (
-                  <SwiperSlide key={pic}>
-                    <img
-                      src={pic}
-                      alt={`${product.type} цвета ${product.color} «${
-                        product.name
-                      }», ${index + 1} из ${product.pictures.length}`}
-                    />
-                  </SwiperSlide>
-                ))}
-                <div className="product__gallery-controls"></div>
-              </Swiper>
-            )}
+    if (productsRequestStatus !== RequestStatus.SUCCESS) {
+      return <div>Loading...</div>;
+    }
 
-            <div className="product__info">
-              {(isDesktop || isTablet) && <Breadcrumbs product={product} />}
-              <h3 className="product__name">{product.name}</h3>
-              <strong
-                className="product__price"
-                aria-label={`Цена ${product.price.value} ${product.price.currency}`}
-              >
-                <span className="product__price--value">
-                  {product.price.value}
-                </span>
-                &nbsp;&#8372;
-              </strong>
-              <p className="product__description">{product.description}</p>
-              <ul className="product__care-info">
-                <li>{product.about.comp}</li>
-                <li>{product.about.wash}</li>
-              </ul>
+    return (
+      <React.Fragment>
+        <main className="main-content" id="main-content">
+          <h1 className="visually-hidden">
+            {`${product.type} цвета ${product.color} «${product.name}»`}
+          </h1>
 
-              <form
-                action="https://echo.htmlacademy.ru"
-                method="post"
-                className="product__add-bag"
-              >
-                <div className="product__size-select">
-                  <label className="visually-hidden" htmlFor="product-size">
-                    Выбрать размер
-                  </label>
-                  <select
-                    className="product__size"
-                    name="product-size"
-                    id="product-size"
-                  >
-                    {Object.entries(product.sizes).map(
-                        ([sizeLabel, sizeValue]) => (
-                          <option
-                            key={sizeLabel}
-                            value={sizeLabel}
-                          >{`${sizeValue.label} - ${sizeValue.stock} шт`}</option>
-                        )
-                    )}
-                  </select>
-                  <svg className="product__select-svg" width="15" height="15">
-                    <use xlinkHref="/img/sprite.svg#icon-caret"></use>
-                  </svg>
-                </div>
-                <button
-                  className="product__submit button button--primary button--glitch"
-                  name="submit"
-                  value="Add to Bag"
-                  type="submit"
+          <section className="product">
+            <h2 className="visually-hidden">Информация о товаре</h2>
+            <div className="product__inner">
+              {isMobile && <Breadcrumbs product={product} />}
+
+              {isDesktop ? (
+                <Swiper
+                  className="product__image-gallery"
+                  direction={desktopSwiperParams.direction}
+                  slidesPerView={desktopSwiperParams.slidesPerView}
+                  allowTouchMove={desktopSwiperParams.allowTouchMove}
+                  spaceBetween={desktopSwiperParams.spaceBetween}
                 >
-                  В корзину
-                </button>
-              </form>
+                  {product.pictures.map((pic, index) => (
+                    <SwiperSlide key={pic}>
+                      <img
+                        src={pic}
+                        alt={`${product.type} цвета ${product.color} «${
+                          product.name
+                        }», ${index + 1} из ${product.pictures.length}`}
+                      />
+                    </SwiperSlide>
+                  ))}
+                  <div className="product__gallery-controls"></div>
+                </Swiper>
+              ) : (
+                <Swiper
+                  className="product__image-gallery"
+                  pagination={mobileSwiperParams.pagination}
+                  a11y={mobileSwiperParams.a11y}
+                  effect={mobileSwiperParams.effect}
+                  direction={mobileSwiperParams.direction}
+                  grabCursor={mobileSwiperParams.grabCursor}
+                  slidesPerView={mobileSwiperParams.slidesPerView}
+                  loop={mobileSwiperParams.loop}
+                  autoHeight={mobileSwiperParams.autoHeight}
+                >
+                  {product.pictures.map((pic, index) => (
+                    <SwiperSlide key={pic}>
+                      <img
+                        src={pic}
+                        alt={`${product.type} цвета ${product.color} «${
+                          product.name
+                        }», ${index + 1} из ${product.pictures.length}`}
+                      />
+                    </SwiperSlide>
+                  ))}
+                  <div className="product__gallery-controls"></div>
+                </Swiper>
+              )}
 
-              <ul className="product__helpers">
-                <li className="product__helper-item">
-                  <Link
-                    to={AppRoute.SIZES.path}
-                    className="product__helper-link"
-                    target="_blank"
+              <div className="product__info">
+                {(isDesktop || isTablet) && <Breadcrumbs product={product} />}
+                <h3 className="product__name">{product.name}</h3>
+                <strong
+                  className="product__price"
+                  aria-label={`Цена ${product.price.value} ${product.price.currency}`}
+                >
+                  <span className="product__price--value">
+                    {product.price.value}
+                  </span>
+                &nbsp;&#8372;
+                </strong>
+                <p className="product__description">{product.description}</p>
+                <ul className="product__care-info">
+                  <li>{product.about.comp}</li>
+                  <li>{product.about.wash}</li>
+                </ul>
+
+                <form className="product__add-bag">
+                  <div className="product__size-select">
+                    <label className="visually-hidden" htmlFor="product-size">
+                    Выбрать размер
+                    </label>
+                    <select
+                      className="product__size"
+                      name="product-size"
+                      id="product-size"
+                      value={currentSize}
+                      onChange={this.handleSizeSelectChange}
+                    >
+                      <option
+                        value="not-selected"
+                        disabled
+                      >Выбрать размер</option>
+                      {Object.entries(product.sizes).map(
+                          ([sizeLabel, sizeValue]) => (
+                            <option
+                              key={sizeLabel}
+                              value={sizeLabel}
+                            >{`${sizeValue.label} - ${sizeValue.stock} шт`}</option>
+                          )
+                      )}
+                    </select>
+                    <svg className="product__select-svg" width="15" height="15">
+                      <use xlinkHref="/img/sprite.svg#icon-caret"></use>
+                    </svg>
+                  </div>
+                  <button
+                    className="product__submit button button--primary button--glitch"
+                    name="submit"
+                    value="Add to Bag"
+                    disabled={currentSize === `not-selected`}
+                    onClick={(evt) => {
+                      evt.preventDefault();
+                      onAddToCartButtonClick(product.id, currentSize);
+                    }}
                   >
+                    В корзину
+                  </button>
+                </form>
+
+                <ul className="product__helpers">
+                  <li className="product__helper-item">
+                    <Link
+                      to={AppRoute.SIZES.path}
+                      className="product__helper-link"
+                      target="_blank"
+                    >
                     Таблица&nbsp;размеров
-                  </Link>
-                </li>
-                <li className="product__helper-item">
-                  <Link
-                    to={AppRoute.DELIVERY.path}
-                    className="product__helper-link"
-                    target="_blank"
-                  >
+                    </Link>
+                  </li>
+                  <li className="product__helper-item">
+                    <Link
+                      to={AppRoute.DELIVERY.path}
+                      className="product__helper-link"
+                      target="_blank"
+                    >
                     Доставка&nbsp;&amp;&nbsp;Возврат
-                  </Link>
-                </li>
-              </ul>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-        </section>
-      </main>
-    </React.Fragment>
-  );
-};
+          </section>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
 
 const mapStateToProps = (state, ownProps) => ({
   productsRequestStatus: getProductsRequestStatus(state),
   product: getProductByAlias(state, ownProps),
 });
 
-export default connect(mapStateToProps)(ProductPage);
+const mapDispatchToProps = (dispatch) => ({
+  onAddToCartButtonClick(productId, size) {
+    dispatch(CartOperations.addProductToCart(productId, size));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductPage);
--- src/components/side-cart/side-cart.tsx
+++ src/components/side-cart/side-cart.tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
+import {Link} from "react-router-dom";
 import {connect} from "react-redux";
 
 import {ActionCreator} from "../../store/app/app";
 import {getIsSideCartOpen} from "../../store/app/selectors";
+import {Operations as CartOperations} from "../../store/cart/cart";
+import {getCartProducts, getCartTotalCost} from "../../store/cart/selectors";
 
-import {PageCategories} from "../../helpers/const";
+import {AppRoute, PageCategories} from "../../helpers/const";
+import {CartProductInterface} from "../../helpers/my-types";
 
 interface SideCartProps {
   currentPage: {
@@ -18,7 +22,10 @@ interface SideCartProps {
     isMobile: boolean;
   };
   isSideCartOpen: boolean;
+  cartProducts: Array<CartProductInterface> | [];
+  cartTotalCost: number;
   onCartCloseEvent(): void;
+  onProductRemoveButtonClick(productId: number, size: string): void;
 }
 
 class SideCart extends React.PureComponent<SideCartProps> {
@@ -50,6 +57,34 @@ class SideCart extends React.PureComponent<SideCartProps> {
     }
   }
 
+  private getCartItems() {
+    const {cartProducts, onProductRemoveButtonClick} = this.props;
+
+    return (cartProducts as Array<CartProductInterface>).map((product) => (
+      <li key={`${product.id}-${product.selectedSize.label}`} className="cart__item">
+        <div className="cart__image-wrapper">
+          <Link to={`${AppRoute.PRODUCT.path}/${product.alias}`} className="cart__image-link">
+            <img className="cart__image" src={product.pictures[0]}
+              alt="Футболка черного цвета «Dont Make Me Beast»" />
+          </Link>
+        </div>
+        <div className="cart__item-info">
+          <h3 className="cart__item-name">
+            <Link to={`${AppRoute.PRODUCT.path}/${product.alias}`} className="cart__item-link">{product.name}</Link>
+          </h3>
+          <p className="cart__item-description">
+            Размер:&nbsp;<span className="cart__item-size--value">{product.selectedSize.label}</span><br />
+            Количество:&nbsp;<span className="cart__item-quantity--value">{product.itemQuantity}</span><br />
+            Цена:&nbsp;<span className="cart__item-price--value">{product.itemTotal}</span>&nbsp;&#8372;
+          </p>
+          <button className="cart__item-remove" type="button" aria-label="Убрать из корзины" onClick={() => {
+            onProductRemoveButtonClick(product.id, product.selectedSize.key);
+          }}>Убрать</button>
+        </div>
+      </li>
+    ));
+  }
+
   componentDidMount() {
     this.handleCartHeight();
 
@@ -73,7 +108,8 @@ class SideCart extends React.PureComponent<SideCartProps> {
   }
 
   render() {
-    const {isSideCartOpen, onCartCloseEvent} = this.props;
+    const {isSideCartOpen, cartProducts, cartTotalCost, onCartCloseEvent} = this.props;
+    const totalProductsAmount = (cartProducts as Array<CartProductInterface>).reduce((acc, product) => product.itemQuantity + acc, 0);
 
     return (
       <React.Fragment>
@@ -84,7 +120,9 @@ class SideCart extends React.PureComponent<SideCartProps> {
           onClick={onCartCloseEvent}
         ></div>
         <div
-          className={`cart ${isSideCartOpen && `cart--open`}`}
+          className={`cart ${isSideCartOpen && `cart--open`} ${
+            cartProducts.length === 0 && `cart--empty`
+          }`}
           id="cart-pop"
           ref={this.myCart}
         >
@@ -97,129 +135,55 @@ class SideCart extends React.PureComponent<SideCartProps> {
                 onClick={onCartCloseEvent}
               ></button>
               <p className="cart__item-quantity">
-                <span className="cart__item-quantity--empty">пусто</span>
+                <span className="cart__item-quantity--empty">empty</span>
                 <small className="cart__item-quantity--fill">
-                  <span className="cart__item-quantity--value">2</span>
-                  &nbsp;вещи
+                  <span className="cart__item-quantity--value">{totalProductsAmount}</span>
+                  &nbsp;{totalProductsAmount === 1 ? `item` : `items`}
                 </small>
               </p>
             </header>
 
             <ul className="cart__list">
-              <li className="cart__item cart__item--empty">
-                <p className="cart__info-empty">
-                  Корзина пуста!
-                  <br />
-                  <a className="cart__info-empty--link" href="catalog.html">
-                    Самое время начать покупки
-                  </a>
-                </p>
-              </li>
-
-              <li className="cart__item">
-                <div className="cart__image-wrapper">
-                  <a className="cart__image-link" href="starina.html">
-                    <img
-                      className="cart__image"
-                      src="/img/products/wycc-h-starina.jpg"
-                      alt="Худи черного цвета «Starina»"
-                    />
-                  </a>
-                </div>
-                <div className="cart__item-info">
-                  <h3 className="cart__item-name">
-                    <a className="cart__item-link" href="starina.html">
-                      Starina
-                    </a>
-                  </h3>
-                  <p className="cart__item-description">
-                    Размер:&nbsp;
-                    <span className="cart__item-size--value">Large</span>
-                    <br />
-                    Количество:&nbsp;
-                    <span className="cart__item-quantity--value">1</span>
-                    <br />
-                    Цена:&nbsp;
-                    <span className="cart__item-price--value">1000.00</span>
-                    &nbsp;&#8381;
-                  </p>
-                  <button
-                    className="cart__item-remove"
-                    type="button"
-                    aria-label="Убрать из корзины"
-                  >
-                    Убрать
-                  </button>
-                </div>
-              </li>
-
-              <li className="cart__item">
-                <div className="cart__image-wrapper">
-                  <a
-                    className="cart__image-link"
-                    href="dont-make-me-beast.html"
-                  >
-                    <img
-                      className="cart__image"
-                      src="/img/products/beast-t-front.jpg"
-                      alt="Футболка черного цвета «Dont Make Me Beast»"
-                    />
-                  </a>
-                </div>
-                <div className="cart__item-info">
-                  <h3 className="cart__item-name">
-                    <a
-                      className="cart__item-link"
-                      href="dont-make-me-beast.html"
-                    >
-                      Dont make me beast
-                    </a>
-                  </h3>
-                  <p className="cart__item-description">
-                    Размер:&nbsp;
-                    <span className="cart__item-size--value">X-Large</span>
-                    <br />
-                    Количество:&nbsp;
-                    <span className="cart__item-quantity--value">1</span>
-                    <br />
-                    Цена:&nbsp;
-                    <span className="cart__item-price--value">1500.00</span>
-                    &nbsp;&#8381;
-                  </p>
-                  <button
-                    className="cart__item-remove"
-                    type="button"
-                    aria-label="Убрать из корзины"
-                  >
-                    Убрать
-                  </button>
-                </div>
-              </li>
+              {
+                cartProducts.length === 0
+                  ? (
+                    <li className="cart__item cart__item--empty">
+                      <p className="cart__info-empty">
+                        Корзина пуста!
+                        <br />
+                        <Link to={AppRoute.CATALOG.path} className="cart__info-empty--link">
+                          Самое время начать покупки
+                        </Link>
+                      </p>
+                    </li>
+                  )
+                  : this.getCartItems()
+              }
             </ul>
 
             <footer className="cart__footer">
               <p className="cart__footer-total">
                 Всего&nbsp;|&nbsp;
-                <span className="cart__footer-total--value">2500.00</span>
-                &nbsp;руб
+                <span className="cart__footer-total--value">{cartTotalCost}</span>
+                &nbsp;грн
               </p>
               <div className="cart__footer-buttons">
-                <a
+                <Link
+                  to={AppRoute.CART.path}
                   className="cart__footer-link"
-                  href="cart.html"
                   aria-label="Перейти на страницу корзины"
                 >
                   В&nbsp;корзину
-                </a>
-                <a
+                </Link>
+                <Link
+                  to={AppRoute.CHECKOUT.path}
                   className="cart__footer-link"
-                  href="404.html"
                   aria-label="Перейти на страницу оформления заказа"
                 >
                   Оформить&nbsp;
                   <br />
                   заказ
-                </a>
+                </Link>
               </div>
             </footer>
           </div>
@@ -231,11 +195,17 @@ class SideCart extends React.PureComponent<SideCartProps> {
 
 const mapStateToProps = (state) => ({
   isSideCartOpen: getIsSideCartOpen(state),
+  cartProducts: getCartProducts(state),
+  cartTotalCost: getCartTotalCost(state),
 });
 
 const mapDispatchToProps = (dispatch) => ({
   onCartCloseEvent() {
     dispatch(ActionCreator.closeSideCart());
+  },
+
+  onProductRemoveButtonClick(productId, size) {
+    dispatch(CartOperations.removeProductFromCart(productId, size));
   },
 });
 
